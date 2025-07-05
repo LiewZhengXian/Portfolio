13c6e6d43f832fc144c7a377f97dfae52bb78a57
Lzx <zhengxianliew107@gmail.com>
2025-07-05 16:07:07 +0800

    Environment Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import  React from 'react'
+import Navbar from './sections/Navbar'
+const App = () => {
+  return (
+    <main className ="max-w-2xl  mx-auto">
+      <Navbar /> 
+    </main>
+    )
+  }
+export default App
